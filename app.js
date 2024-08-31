deb5a2092a7e461acae1679b3613bb87f336b540
meet-tola <wasiusikiru7@gmail.com>
2024-08-31 13:00:10 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+import { Server } from "socket.io";
+
+const io = new Server({
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+let onlineUser = [];
+
+const addUser = (userId, socketId) => {
+  const userExits = onlineUser.find((user) => user.userId === userId);
+  if (!userExits) {
+    onlineUser.push({ userId, socketId });
+  }
+};
+
+const removeUser = (socketId) => {
+  onlineUser = onlineUser.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return onlineUser.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+
+  socket.on("newUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", onlineUser); 
+  });
+
+  socket.on("sendMessage", ({ receiverId, message }) => {
+    const receiver = getUser(receiverId);
+    if (receiver) {
+      io.to(receiver.socketId).emit("getMessage", message);
+    } else {
+      console.log(`User ${receiverId} is not online`);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    removeUser(socket.id);
+    io.emit("getUsers", onlineUser); 
+  });
+});
+
+
+io.listen("4000");
